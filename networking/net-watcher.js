da22a1cc5eab7de75e013307000d0a96ea6b47d6
Manu Uzkudun <uzkudun@mdpi.com>
2016-08-12 11:15:29 +0200

    Repository initialized

---
+++ networking/net-watcher.js
@@ -0,0 +1,34 @@
+'use strict';
+const
+	fs = require('fs'),
+	net = require('net'),
+
+	filename = process.argv[2],
+
+	server = net.createServer(function(connection) {
+		// reporting to the server
+		console.log('Subscriber connected.');
+		// reporting to the client
+		connection.write("Now watching '" + filename + "' for changes...\n");
+
+		// watcher setup
+		let watcher = fs.watch(filename, function() {
+			// Send a message to the client when the file changes
+			connection.write("File '" + filename + "' changed: " + Date.now() + "\n");
+		});
+
+		// Send a message to the console when client disconects
+		connection.on('close', function() {
+			console.log('Subscriber disconnected.');
+			watcher.close();
+		});
+
+	});
+
+	if (!filename) {
+		throw Error('No target filename was specified.');
+	}
+
+	server.listen(5432, function() {
+		console.log('Listening for subscribers...');
+	});
